f349d174c24c26c6fc6676289f69e2c1a21e68a4
DenZaiyy <denzaiyy@outlook.fr>
2023-09-17 01:30:43 +0200

    add: second course (type object)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
 
 function App() {
+    const personName = {
+        first: "Bruce",
+        last: "Wayne",
+    };
+
     return (
         <>
             <div className="App">
                 <Greet name="denZ" messageCount={10} isLoggedIn={false} />
+                <Person name={personName} />
             </div>
         </>
     );
