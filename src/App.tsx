458fda0d4c00e336ec19d2de0116b318c618ef23
DenZaiyy <denzaiyy@outlook.fr>
2023-09-17 02:06:21 +0200

    add: props style with type CSSProperties

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
@@ -45,8 +46,10 @@ function App() {
                         console.log("Button clicked", event, id)
                     }
                 />
-
                 <Input value="" handleChange={(event) => console.log(event)} />
+                <Container
+                    styles={{ border: "1px solid black", padding: "1rem" }}
+                />
             </div>
         </>
     );
