1880c1b33f868b6548c389e28b4c03c4a7f02ba2
DenZaiyy <denzaiyy@outlook.fr>
2023-09-18 17:42:46 +0200

    update: useState type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -5,16 +5,14 @@ type AuthUser = {
     email: string
 }
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = () => {setUser({name: 'denZ', email: 'denz@gmail.com'})}
-    const handleLogout = () => {setUser(null)}
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
